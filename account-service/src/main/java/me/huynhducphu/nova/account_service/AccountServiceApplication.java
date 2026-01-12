ff4225f07867b8ef2a986c698392e14641b1e2d4
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-01-12 15:49:53 +0700

    remove test controller

--- account-service/src/main/java/me/huynhducphu/nova/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/me/huynhducphu/nova/account_service/AccountServiceApplication.java
@@ -1,14 +1,11 @@
 package me.huynhducphu.nova.account_service;
 
-import me.huynhducphu.nova.account_service.config.common.ContactInfoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@EnableConfigurationProperties(ContactInfoConfig.class)
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
