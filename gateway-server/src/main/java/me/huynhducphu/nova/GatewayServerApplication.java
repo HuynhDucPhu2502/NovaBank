0980d462526caac27d818fdd0eafa8652d417f0a
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-01 11:48:24 +0700

    feat(gateway): Add rate limiting and circuit breaker
    Implement Redis-backed rate limiting and Resilience4j circuit breakers for all proxied services in the GatewayServerApplication. Add a GlobalFallbackController for error handling. Update pom.xml for new dependencies.

--- gateway-server/src/main/java/me/huynhducphu/nova/GatewayServerApplication.java
+++ gateway-server/src/main/java/me/huynhducphu/nova/GatewayServerApplication.java
@@ -2,9 +2,14 @@ package me.huynhducphu.nova;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class GatewayServerApplication {
@@ -16,19 +21,71 @@ public class GatewayServerApplication {
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
+
+                // ACCOUNT SERVICE
                 .route(p -> p
                         .path("/novabank/account-service/**")
-                        .filters(f -> f.rewritePath("/novabank/account-service/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/novabank/account-service/(?<segment>.*)", "/${segment}")
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())
+                                )
+                                .circuitBreaker(c -> c
+                                        .setName("accountCB")
+                                        .setFallbackUri("forward:/fallback/global-error")
+                                )
+                        )
                         .uri("lb://ACCOUNT-SERVICE"))
+
+                // LOAN SERVICE
                 .route(p -> p
                         .path("/novabank/loan-service/**")
-                        .filters(f -> f.rewritePath("/novabank/loan-service/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/novabank/loan-service/(?<segment>.*)", "/${segment}")
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())
+                                )
+                                .circuitBreaker(c -> c
+                                        .setName("loanCB")
+                                        .setFallbackUri("forward:/fallback/global-error")
+                                )
+                        )
                         .uri("lb://LOAN-SERVICE"))
+
+                // CARD SERVICE
                 .route(p -> p
                         .path("/novabank/card-service/**")
-                        .filters(f -> f.rewritePath("/novabank/card-service/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/novabank/card-service/(?<segment>.*)", "/${segment}")
+                                .requestRateLimiter(config -> config
+                                        .setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver())
+                                )
+                                .circuitBreaker(c -> c
+                                        .setName("cardCB")
+                                        .setFallbackUri("forward:/fallback/global-error")
+                                )
+                        )
                         .uri("lb://CARD-SERVICE"))
+
+
                 .build();
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(20, 40);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono
+                .just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress())
+                        .getAddress()
+                        .getHostAddress()
+                );
     }
 
 }
---
+++ gateway-server/src/main/java/me/huynhducphu/nova/controller/GlobalFallbackController.java
@@ -0,0 +1,23 @@
+package me.huynhducphu.nova.controller;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Admin 2/1/2026
+ *
+ **/
+@RestController
+public class GlobalFallbackController {
+
+//    @GetMapping("/fallback/global-error")
+//    public ResponseEntity<@NonNull String> globalError() {
+//        return ResponseEntity
+//                .status(HttpStatus.SERVICE_UNAVAILABLE)
+//                .body("Hệ thống đang bận, vui lòng thử lại sau.");
+//    }
+
+}
